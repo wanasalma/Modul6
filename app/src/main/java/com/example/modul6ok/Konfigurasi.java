fd5b418c20e5a518a21a5a570c2f312366a2face
wanasalma <54799315+wanasalma@users.noreply.github.com>
2020-05-14 22:46:20 -0700

    pertama

---
+++ app/src/main/java/com/example/modul6ok/Konfigurasi.java
@@ -0,0 +1,23 @@
+package com.example.modul6ok;
+
+public class Konfigurasi {
+    public static final String URL_ADD = "https://bimaalfayyadl.000webhostapp.com/mobilemodul6/insert.php";
+    public static final String URL_GET_ALL = "https://bimaalfayyadl.000webhostapp.com/mobilemodul6/read.php";
+    public static final String URL_GET_MHS = "https://bimaalfayyadl.000webhostapp.com/mobilemodul6/select.php";
+    public static final String URL_UPDATE_MHS = "https://bimaalfayyadl.000webhostapp.com/mobilemodul6/update.php";
+    public static final String URL_DELETE_MHS = "https://bimaalfayyadl.000webhostapp.com/mobilemodul6/delete.php";
+    //Dibawah ini merupakan Kunci yang akan digunakan untuk mengirim permintaan ke Script PHP
+    public static final String KEY_MHS_ID = "id";
+    public static final String KEY_MHS_NAMA = "nama";
+    public static final String KEY_MHS_JURUSAN = "jurusan";
+    public static final String KEY_MHS_EMAIL = "email";
+    //JSON Tags
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_JURUSAN = "jurusan";
+    public static final String TAG_EMAIL = "email";
+    //ID Mahasiswa
+    //mhs itu singkatan dari Mahasiswa
+    public static final String MHS_ID = "mhs_id";
+}
---
+++ app/src/main/java/com/example/modul6ok/list_item.java
@@ -0,0 +1,14 @@
+package com.example.modul6ok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class list_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_item);
+    }
+}
